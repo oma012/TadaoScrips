335131d06b6a1325006c1056d763554d2fe2a3bb
oma012 <oma_012@msn.com>
2020-09-10 23:04:33 -0700

    Update CustomIntro.js

--- CustomIntro.js
+++ CustomIntro.js
@@ -12,28 +12,37 @@
  * @max 6000
  * @default 15
  *
+ * @param MapaIntro
+ * @type number
+ * @desc El mapa que usaras para crear el custom intro y solo para eso
+ * @min 1
+ * @max 100
+ * @default 3
+ *
+ * @param MapaIncial
+ * @type number
+ * @desc El mapa donde iniciara tu juego.
+ * @min 1
+ * @max 100
+ * @default 1
+ *
  * @help este pruging hace que te saltes el intro del juego para que puedas crear
  * tu propio intro nota para que se mire bien tienes que hacer los siguientes pasos
  * 1- en la base de datos, en Sistema, en Opciones palomea iniciar trasparente
  * 2- crea un evento automatico en el mapa donde inicia el jugador donde crees tu 
- * intro custom
+ * intro custom (lo especificas en los parametros del script)
  * 3- llena tu evento custom con las herramientas de RPG maker
  * 4- al finalizar todos tu intro en el evento crea un contenido
  * avanzado de la pagina 3 llamado Script y se abrira un 
  * cuadro para escribir ahi pega esto :
- * "$dataSystem.startMapId = 1; SceneManager.goto(Scene_Title);"
- * el 1 representa el mapa en el que iniciara el jugador al poner new game
- * siendo el 1 MAP001, el 2 el MAP002 etc, no pongas el mapa donde tienes 
- * tu Inicio custom por que si no, se hara un loop y nunca podras jugar por ejemplo
- * yo en mi mapa 3 cree un codigo custom para intro, de ahi ya termino,
- * el codigo script que esta arriba hace que el juego sepa que el mapa inicial 
- * ahora es el 1 y no el 3 y que me mande a la pantalla de titulo, asi al
- * poner new game no me mandara al mapa 3 donde tengo la pocicion inicial del 
- * heroe si no al 1 ahora en el mapa donde inicia el new game tenemos que crear 
- * otro evento automatico para señalar que la trasparencia del jugador esta 
- * apagada, o si tambien tienes un inicio de juego custom pudes diseñarlo ahi,
- * NOTA!!! importante el jugador aparecera en las mismcas cordenadas que en donde
- * este en el mapa de inicio ejemplo si en tu mapa de inicio el heroe esta en 
+ * "SceneManager.goto(Scene_Title);"
+ * 5- listo ya tienes tu intro, al poner new game te mandara al mapa definido
+ * como mapa inicial en los parametros del script
+ * para los mapas sepan que el mapa 1 correspondria al mapa
+ * MAP001, el 2 el MAP002 etc, no pongan el mapa de intro y el custom en el mismo
+ * mapa por que se hara un loop y nunca podras jugar.
+ * NOTA importante el jugador aparecera en las mismcas cordenadas que en donde
+ * este en el mapa del intro ejemplo si en tu mapa de intro el heroe esta en 
  * (5,5) cuando hagas new game y te manden a el otro mapa el heroe aparecera en el
  * (5,5) del nuevo mapa.
  * 
@@ -44,17 +53,29 @@
     var miParametro = PluginManager.parameters('CustomIntro');
     var MaxTime = (miParametro["TiempoEspera"]*60);
     var timeintittle = 0;
+    
+Scene_Title.prototype.commandNewGame = function() {
+    $dataSystem.startMapId = miParametro["MapaIncial"];
+    DataManager.setupNewGame();
+    this._commandWindow.close();
+    this.fadeOutAll();     
+    SceneManager.goto(Scene_Map);
+};
+
 
 Scene_Title.prototype.update = function() {
     timeintittle = timeintittle+1;
     if (timeintittle > MaxTime)
 	{
+		$dataSystem.startMapId = miParametro["MapaIntro"];
+    		DataManager.setupNewGame();
+    		this._commandWindow.close();
+    		this.fadeOutAll();
 		AudioManager.stopBgm();
     		AudioManager.stopBgs();
     		AudioManager.stopMe();
 		AudioManager.stopSe();
-		timeintittle = 0;
-		$dataSystem.startMapId = 0;
+		timeintittle = 0;   		
 		SceneManager.goto(Scene_Map);
 	}
     if (TouchInput.isPressed()||Input.isPressed('cancel')||Input.isPressed('dash')||Input.isPressed('menu')||Input.isPressed('ok')||Input.isPressed('up')||Input.isPressed('down')||Input.isPressed('left')||Input.isPressed('right'))
@@ -85,8 +106,27 @@ Scene_Boot.prototype.start = function() {
     this.updateDocumentTitle();
 };
 
+Graphics._updateVideo = function() {
+    if (!this._isFullScreen())
+    {
+    //this._video.width = 1105;
+    //this._video.height = 1945;
+    //this._video.width = window.screen.width+30;
+    //this._video.height = window.screen.height+30;
+    this._video.width = window.screen.height+30;//work 1080p
+    this._video.height = window.screen.width;//
+    }
+    else
+    {
+    this._video.width = this._width;
+    this._video.height = this._height;
+    }
+    this._video.style.zIndex = 2;
+    this._centerElement(this._video);
+};
+
 Graphics.isVideoPlaying = function() {
-    if (Input.isPressed('cancel'))
+    if (Input.isTriggered('cancel'))
     { 
       this._videoLoading = false;
       this._updateVisibility(false);
