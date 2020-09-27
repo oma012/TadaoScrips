639d1cab32b6b60a0a2ce9e6fc9d1dcee20314a2
oma012 <oma_012@msn.com>
2020-09-27 15:40:24 -0700

    Update CustomCursor.js

--- CustomCursor.js
+++ CustomCursor.js
@@ -6,21 +6,22 @@
  * @author oma012
  *
  * @help Cambia el cursor del juego
- * solo crea una carpeta donde tienes el ejecutable
- * del juego que se llame Cursor y pon las 5 imagenes
+ * pon las 5 imagenes en la carpeta img/system
  * para hacer la animacion de tu cursor y que sean
- * 32X32, cuando tenga tiempo lo paso a un archivo 
+ * 32X32 y llamalas 0001,0002,0003,0004 y 0005 
+ * en el mismo orden te tu animacion, cuando 
+ * tenga tiempo lo paso a un archivo 
  * prite y/o que tu puedas poner las imagenes
  * que quieras y cuantos frames quiers que esten
  * cada imagen en pantalla
  */
 
 var imagesmycursor = [
-'Cursors/0001.png',
-'Cursors/0002.png',
-'Cursors/0003.png',
-'Cursors/0004.png',
-'Cursors/0005.png'
+'img/system/0001.png',
+'img/system/0002.png',
+'img/system/0003.png',
+'img/system/0004.png',
+'img/system/0005.png'
 ];
 var timeframecursor = 0;
 var myframecursor = 0;
